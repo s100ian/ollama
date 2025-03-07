5f0dbccb7087539e4519acd1ec6bc7c20af6ff7f
stoyan-atanasov-dreamix <stoian.atanasov@gmail.com>
2025-03-07 22:18:45 +0200

    Add logging to PersonService methods

--- src/main/java/dev/stoyan/ollama/person/PersonService.java
+++ src/main/java/dev/stoyan/ollama/person/PersonService.java
@@ -1,5 +1,6 @@
 package dev.stoyan.ollama.person;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +9,7 @@ import java.util.NoSuchElementException;
 import java.util.Set;
 
 @Service
+@Slf4j
 public class PersonService {
     private static final Collection<PersonDTO> PERSONS = Set.of(
             new PersonDTO("Stoyan", 25, Set.of("Java", "Spring Boot", "meat balls")),
@@ -17,16 +19,19 @@ public class PersonService {
 
     @Tool(description = "Get the expertise of a person by name")
     public Set<String> getExpertise(final String name) {
-        return PERSONS.stream().filter(p -> p.name().equals(name)).findFirst().map(PersonDTO::expertise).orElseThrow(() -> new NoSuchElementException("Who? I don't know that person."));
+        log.info("getExpertise called with name: {}", name);
+        return PERSONS.stream().filter(p -> p.name().equals(name)).findFirst().map(PersonDTO::expertise).orElseThrow(() -> new NoSuchElementException("Who? I don't know that person " + name + "."));
     }
 
     @Tool(description = "Get the age of a person by name")
     public int getAge(final String name) {
-        return PERSONS.stream().filter(p-> p.name().equals(name)).findFirst().map(PersonDTO::age).orElseThrow(() -> new NoSuchElementException("Who? I don't know that person."));
+        log.info("getAge called with name: {}", name);
+        return PERSONS.stream().filter(p -> p.name().equals(name)).findFirst().map(PersonDTO::age).orElseThrow(() -> new NoSuchElementException("Who? I don't know that person " + name + "."));
     }
 
     @Tool(description = "Get the names of all persons")
     public Collection<String> getPersonNames() {
+        log.info("getPersonNames called");
         return PERSONS.stream().map(PersonDTO::name).toList();
     }
 
